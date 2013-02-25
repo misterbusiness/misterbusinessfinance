21f74d3896a1ea395578fbf0fd08866413a22462
misterbusiness <misterbusinesscorp@gmail.com>
2013-02-25 20:08:46 -0300

    Commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Misterbusinessfinance::Application.config.secret_token = 'ae1ca12f21d013bd02be476f241f710be15afd9ea5a1e336e8f7091876dd3f430fbf94180a777e7a7cf5b18bc47d8ddcd7da9d1d382f7d91441d78056f15c2a3'
