da85b278cb089a6fd03a42d7584d64b49e7a01cb
joaohenriqueabreu <joaohenriqueabreu@gmail.com>
2013-03-05 20:41:37 -0300

    Teste commit Git conta joaohenriqueabreu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Misterbusinessfinance::Application.routes.draw do
   resources :lancamentos
+  
+  
 
 
   # The priority is based upon order of creation:
@@ -51,7 +53,7 @@ Misterbusinessfinance::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'lancamentos#index'
+  #root :to => 'lancamentos#index'
 
   # See how all your routes lay out with "rake routes"
 
