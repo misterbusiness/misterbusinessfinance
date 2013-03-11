b6e6c339cbb9c3690cf7e53874a6747d97be6d76
joaohenriqueabreu <joaohenriqueabreu@gmail.com>
2013-03-11 20:42:44 -0300

    Teste git push

--- app/models/parcela.rb
+++ app/models/parcela.rb
@@ -1,6 +1,8 @@
 class Parcela < ActiveRecord::Base
   attr_accessible :num_parcelas
   
+  validates :num_parcelas, :presence => true
+  
   has_many :parcela_lancamento, :dependent => :destroy
   has_many :lancamento, :through => :parcela_lancamento
 end
