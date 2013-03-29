5e4b50d35f26bb7d86e29cd94349d5e74aab8a5c
joaohenriqueabreu <joaohenriqueabreu@gmail.com>
2013-03-29 16:59:58 -0300

    Heroku test2

--- db/migrate/20130310143727_change_tipo_column_to_lancamento.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTipoColumnToLancamento < ActiveRecord::Migration
-  def up
-    change_column :lancamentos, :tipo, :integer
-    change_column :lancamentos, :status, :integer
-  end
-
-  def down
-  end
-end
