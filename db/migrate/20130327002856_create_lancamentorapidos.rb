249b7cbc55e9ec5c997b1c58e580609ad876f31e
joaohenriqueabreu <joaohenriqueabreu@gmail.com>
2013-03-29 17:16:39 -0300

    Heroku test7

--- db/migrate/20130327002856_create_lancamentorapidos.rb
+++ db/migrate/20130327002856_create_lancamentorapidos.rb
@@ -1,8 +1,8 @@
 class CreateLancamentorapidos < ActiveRecord::Migration
   def change
-    create_table :lancamentorapidos do |t|
+   # create_table :lancamentorapidos do |t|
 
-      t.timestamps
+   #   t.timestamps
     end
   end
 end
