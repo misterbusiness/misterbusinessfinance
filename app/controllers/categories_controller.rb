52aa0e8416b47a6fa656240c163d6a45667c2cd1
Daniel Wolbert <dxwolb@gmail.com>
2013-04-14 18:14:22 -0300

    Inclusão do Select2 para categoria pai

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -47,7 +47,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path}
         format.json { render json: @category, status: :created, location: @category }
       else
         format.html { render action: "new" }
@@ -63,7 +63,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.update_attributes(params[:category])
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to categories_path}
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
