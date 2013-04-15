e9e710892919b140a5a0487ec4411377c36f4a69
Daniel Wolbert <dxwolb@gmail.com>
2013-04-15 17:22:58 -0300

    Melhorias na Árvore de Categorias para exibir todos os nós

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,76 @@
 module CategoriesHelper
-end
+
+  def display_categories_tree(categories)
+
+    ret = "<ul>"
+
+    for category in categories
+      if category.parent_id.nil?
+        ret += "<li>"
+        ret += category.descricao
+        ret += '  '
+        ret += link_to 'Editar', edit_category_path(category)
+        ret += find_all_subcategories(category)
+        ret += "</li>"
+      end
+
+    end
+
+    ret += "</ul>"
+
+  end
+
+  def find_all_subcategories(category)
+
+    if category.children.size > 0
+
+      ret = '<ul>'
+       category.children.each {  |subcat|
+
+         if subcat.children.size > 0
+           ret +=  '<li>'
+           ret += subcat.descricao
+           ret += '  '
+           ret += link_to 'Editar', edit_category_path(subcat)
+           ret += find_all_subcategories(subcat)
+
+           ret +=  '</li>'
+
+         else
+
+           ret +=  '<li>'
+           ret += subcat.descricao
+           ret += '  '
+           ret += link_to 'Editar', edit_category_path(subcat)
+
+           ret +=  '</li>'
+
+         end
+
+
+
+
+       }
+
+
+      ret += '</ul>'
+
+      else
+
+        ret = ''
+
+      end
+
+
+
+
+
+
+
+    end
+
+
+  end
+
+
+
