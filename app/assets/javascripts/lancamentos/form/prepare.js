3a58ab9d1ce7fe6614070651f985cf28e3a5b048
joaohenriqueabreu <ffaa132333>
2013-07-08 18:31:57 -0300

    Changing form load schema.

---
+++ app/assets/javascripts/lancamentos/form/prepare.js
@@ -0,0 +1,22 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: administrator
+ * Date: 08/07/13
+ * Time: 18:24
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+    prepareForm();
+});
+
+function prepareForm()
+{
+    //prepareTipo();
+    //prepareDescricao();
+    //prepareDatavencimento();
+    //prepareValor();
+    prepareCategory();
+    //prepareCentrodecusto();
+    //prepareAgendamento();
+    //prepareParcelamento();
+}
