ee825066d3d8f2f08c714dded2eb6c4bc45f3790
joaohenriqueabreu <joaohenriqueabreu@gmail.com>
2013-08-17 15:29:30 -0300

    Working datatable with button filters

--- app/controllers/lancamentos_controller.rb
+++ app/controllers/lancamentos_controller.rb
@@ -6,14 +6,18 @@ require 'roo'
 class LancamentosController < ApplicationController
   include ApplicationHelper
   include LancamentosHelper
+  include ActionView::Helpers::NumberHelper
 
   before_filter :load
 
   # GET /lancamentos/filter
   # GET /lancamentos/filter.json
 
-  def filter
-    #@lancamentos = build_query
+  #default params
+
+
+  # Testando a funcionalidade de levar apenas os dados para o cliente
+  def filter_no_server
 
     # Build Query functionality
     query = Lancamento.unscoped
@@ -21,10 +25,15 @@ class LancamentosController < ApplicationController
     #query = Lancamento.padrao
     query = query.scoped_by_centrodecusto_id(params[:centrodecusto]) unless  params[:centrodecusto].nil?
     query = query.scoped_by_category_id(params[:categoria]) unless  params[:categoria].nil?
-    query = query.por_descricao('%' + params[:descricao] + '%') unless params[:descricao].nil?
     query = query.scoped_by_status_cd(params[:status]) unless params[:status].nil?
-    query = query.a_partir_de(params[:datavencimentode]) unless params[:datavencimentode].nil?
-    query = query.ate(params[:datavencimentoate]) unless params[:datavencimentoate].nil?
+
+    query = query.por_descricao('%' + params[:filtro_descricao] + '%') unless params[:filtro_descricao].blank?
+    query = query.a_partir_de(params[:filtro_vencimento_ini]) unless params[:filtro_vencimento_ini].blank?
+    query = query.ate(params[:filtro_vencimento_final]) unless params[:filtro_vencimento_final].blank?
+
+    #query = query.por_descricao('%' + params[:descricao] + '%') unless params[:descricao].nil?
+    #query = query.ate(params[:datavencimentoate]) unless params[:datavencimentoate].nil?
+    #query = query.a_partir_de(params[:datavencimentode]) unless params[:datavencimentode].nil?
 
     if (params[:receita] == 'S' and params[:despesa] == 'N')
       query = query.receitas
@@ -46,20 +55,15 @@ class LancamentosController < ApplicationController
 
     end
 
-    number_of_records = query.count
+    @lancamentos = query
 
-    # Fixed to accept datatable params
-    if params[:sEcho].nil? then
-      @lancamentos = query.paginate(:page => "1", :per_page => 10)
-    else
-      @lancamentos = query.paginate(:page => params[:sEcho], :per_page => 10)
-    end
 
     # Teste retorno json
     json_rows = Array.new
     @lancamentos.each do |serie|
       json_row = Array.new
-      json_row.push("")
+      json_row.push("<a href='#{estornar_lancamento_path(serie.id)}' class='btn'>E</a>")
+      json_row.push("<a href='#{quitar_lancamento_path(serie.id)}' class='btn'>Q</a>")
       json_row.push(serie.descricao)
       json_row.push(serie.datavencimento)
       json_row.push(serie.dataacao)
@@ -71,9 +75,100 @@ class LancamentosController < ApplicationController
     end
 
     render :json => {
+        :aaData => json_rows
+    }
+  end
+
+
+  # Testando a funcionalidade de processamento completo pelo servidor
+  def filter
+
+    if params[:filtro_command] == "default_filter"
+      @lancamento = Lancamento.padrao.paginate(:page => 1, :per_page => 10)
+    else
+
+      # Params collect default
+      per_page = 10
+      curr_page = 1
+      start_of_page = 0
+      columns = {"2" => "descricao", "3" => "datavencimento",
+                 "4" => "dataacao", "5" => "valor", "6" => "category_id",
+                 "7" => "centrodecusto_id", "8" => "status_cd"}
+      sort_col = "datavencimento"
+      sort_direction = "asc"
+
+      # Build Query functionality
+      query = Lancamento.unscoped
+
+      query = query.scoped_by_centrodecusto_id(params[:centrodecusto]) unless  params[:centrodecusto].nil?
+      query = query.scoped_by_category_id(params[:categoria]) unless  params[:categoria].nil?
+      query = query.scoped_by_status_cd(params[:status]) unless params[:status].nil?
+
+      query = query.por_descricao('%' + params[:filtro_descricao] + '%') unless params[:filtro_descricao].blank?
+      query = query.a_partir_de(params[:filtro_vencimento_ini]) unless params[:filtro_vencimento_ini].blank?
+      query = query.ate(params[:filtro_vencimento_final]) unless params[:filtro_vencimento_final].blank?
+
+      #query = query.por_descricao('%' + params[:descricao] + '%') unless params[:descricao].nil?
+      #query = query.ate(params[:datavencimentoate]) unless params[:datavencimentoate].nil?
+      #query = query.a_partir_de(params[:datavencimentode]) unless params[:datavencimentode].nil?
+
+      if (params[:filtro_receita] == 'S' and params[:filtro_despesa] == 'N')
+        query = query.receitas
+
+      elsif (params[:filtro_receita] == 'N' and params[:filtro_despesa] == 'S')
+        query = query.despesas
+      end
+
+      if not params[:valor].nil? then
+        case params[:seletorvalor]
+          when "="
+            query = query.valor_igual(params[:valor])
+          when "<"
+            query = query.valor_menor(params[:valor])
+          when ">"
+            query = query.valor_maior(params[:valor])
+        end
+
+      end
+
+      number_of_records = query.count
+
+      start_of_page = params[:iDisplayStart] unless params[:iDisplayStart].nil?
+
+      # Fixed to accept datatable params
+      per_page = params[:iDisplayLength] unless params[:iDisplayLength].nil?
+      curr_page = ((start_of_page.to_f+1) / per_page.to_f).ceil unless per_page == "0"
+      sort_col = columns[params[:iSortCol_0]] unless params[:iSortCol_0].nil?
+      sort_direction = params[:sSortDir_0] unless params[:sSortDir_0].nil?
+
+      query = query.order("#{sort_col} #{sort_direction}")
+
+      @lancamentos = query.paginate(:page => curr_page, :per_page => per_page)
+
+    end
+    # Teste retorno json
+    json_rows = Array.new
+    @lancamentos.each do |serie|
+      json_row = Array.new
+      json_row.push("<a href='#{estornar_lancamento_path(serie.id)}' class='btn'>E</a>")
+      json_row.push("<a href='#{quitar_lancamento_path(serie.id)}' class='btn'>Q</a>")
+      json_row.push(serie.descricao)
+      json_row.push(!serie.datavencimento.nil? ? serie.datavencimento.strftime('%d-%m-%Y') : nil)
+      json_row.push(!serie.dataacao.nil? ? serie.dataacao.strftime('%d-%m-%Y') : nil)
+      serie.receita? ? sinal = "+" : sinal = "-"
+      json_row.push(number_to_currency(("#{sinal}#{serie.valor}").to_f, precision: 2, unit: "R$ "))
+      json_row.push(serie.category.descricao)
+      json_row.push(serie.centrodecusto.descricao)
+      json_row.push(serie.status)
+      json_rows.push(json_row)
+    end
+
+    render :json => {
         :sEcho => params[:sEcho],
         :iTotalRecords => number_of_records,
         :iTotalDisplayRecords => number_of_records,
+        #:iDisplayStart => params[:iDisplayStart],
+        #:iDisplayLength => params[:iDisplayLength],
         :aaData => json_rows
     }
 
